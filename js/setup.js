7c7a613d51fc4148e2daf9561102b977f8b35142
GolotaRoman <roman.golota11@gmail.com>
2020-02-03 15:49:48 +0200

    Код исправлен согласно замечаниям

--- js/setup.js
+++ js/setup.js
@@ -1,9 +1,31 @@
 'use strict';
 
-var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)'];
-var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARD_NAMES = ['Иван',
+  'Хуан Себастьян', 'Мария',
+  'Кристоф', 'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон'];
+
+var WIZARD_SURNAMES = ['да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг'];
+
+var COAT_COLOR = ['rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)'];
+
+var EYES_COLOR = ['black',
+  'red',
+  'blue',
+  'yellow',
+  'green'];
 
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
@@ -14,20 +36,20 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
     .content
     .querySelector('.setup-similar-item');
 
-// eslint-disable-next-line no-shadow
-var createRandomWizard = function (WIZARD_NAMES, WIZARD_SURNAMES, COAT_COLOR, EYES_COLOR) {
-  var wizards = [];
+var wizards = [];
+
+var createRandomWizard = function (NAMES, SURNAMES, COLOR, EYESCOLOR) {
   for (var i = 0; i < 4; i++) {
     wizards.push({
-      name: WIZARD_NAMES[Math.floor(Math.random() * WIZARD_NAMES.length)] + ' ' + WIZARD_SURNAMES[Math.floor(Math.random() * WIZARD_SURNAMES.length)],
-      coatColor: COAT_COLOR[Math.round(Math.random() * COAT_COLOR.length)],
-      eyesColor: EYES_COLOR[Math.round(Math.random() * EYES_COLOR.length)]
+      name: NAMES[Math.floor(Math.random() * NAMES.length)] + ' ' + SURNAMES[Math.floor(Math.random() * SURNAMES.length)],
+      coatColor: COLOR[Math.round(Math.random() * COLOR.length)],
+      eyesColor: EYESCOLOR[Math.round(Math.random() * EYESCOLOR.length)]
     });
   }
   return wizards;
 };
 
-var wizards = createRandomWizard(WIZARD_NAMES, WIZARD_SURNAMES, COAT_COLOR, EYES_COLOR);
+wizards = createRandomWizard(WIZARD_NAMES, WIZARD_SURNAMES, COAT_COLOR, EYES_COLOR);
 
 var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
